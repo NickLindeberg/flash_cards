a2a5b0c305076496c6b3ed1796d5ec7b94bf58d1
Nick Lindeberg <lindebergnick@gmail.com>
2019-05-06 09:08:19 -0600

    Removes extra pry

--- test/card_test.rb
+++ test/card_test.rb
@@ -12,7 +12,7 @@ class CardTest < Minitest::Test
 
   def test_it_has_a_question
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-require "pry"; binding.pry
+
     assert_equal "What is the capital of Alaska?", card.question
   end
 
